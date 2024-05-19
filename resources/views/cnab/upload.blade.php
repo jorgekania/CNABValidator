9313764e18fc1844a975bbea2f038838e95c4c43
Jorge Kania <jorgefkania@gmail.com>
2024-05-19 13:03:40 -0300

    simple format form upload

--- resources/views/cnab/upload.blade.php
+++ resources/views/cnab/upload.blade.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Upload CNAB</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <style>
         body {
@@ -13,34 +16,50 @@
         }
     </style>
 </head>
-<body>
-    <form action="{{ route('cnab.validate') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div>
-            <label for="file">Arquivo CNAB:</label>
-            <input type="file" name="file" id="file" required>
-        </div>
-        <div>
-            <button type="submit" name="submit">Validar</button>
-        </div>
-    </form>
-    @if ($errors->any())
-        <div>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
 
-    @if (session('success'))
-        <div>
-            {{ session('success') }}
+<body>
+    <div class="container mt-4">
+        <div class="row justify-content-center">
+            <div class="my-4 text-center">
+                <h1>Validador de arquivo CNAB</h1>
+            </div>
+            <form action="{{ route('cnab.validate') }}" method="POST" enctype="multipart/form-data"
+                class="col-lg-12 w-50">
+                @csrf
+                <div class="mb-3">
+                    <label for="file" class="form-label fw-bold">Arquivo CNAB:</label>
+                    <input type="file" class="form-control" id="file" name="file" required>
+                </div>
+                <div class="d-flex justify-content-end">
+                    <button type="submit" class="btn btn-primary">Validar</button>
+                </div>
+            </form>
+            <div class="col-lg-12">
+                @if ($errors->any())
+                    <div class="alert alert-danger mt-3" role="alert">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                @if (session('success'))
+                    <div class="alert alert-success mt-3" role="alert">
+                        {{ session('success') }}
+                    </div>
+                @endif
+            </div>
+            <div class="mt-4 text-center text-sm text-gray-500 col-lg-12">
+                Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+            </div>
         </div>
-    @endif
-    <div class="mt-4 ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
